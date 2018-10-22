db62bd6e4f080879ea53cc26ba3f60c493b43e61
Dustin Schie <dustin@schie.io>
2018-10-22 15:39:17 -0400

    :art: removing unused imports and prettier-izing things

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-const { GraphQLServer } = require('graphql-yoga')
-const axios = require('axios')
-
+const { GraphQLServer } = require('graphql-yoga');
 require('dotenv').config();
 
 const opts = {
-  port: process.env.PORT
-}
+  port: process.env.PORT || 4000
+};
 
-const resolvers = require('./resolvers')
+const resolvers = require('./resolvers');
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
   opts
-})
+});
 
-server.start(() => console.log(`Server is running at http://localhost:${opts.port}`))
+server.start(() => console.log(`Server is running at http://localhost:${opts.port}`));
