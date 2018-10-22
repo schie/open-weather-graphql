cc7ab6b9836a880ab886cc58ae73d618fcf71c61
Dustin Schie <dustin@schie.io>
2018-10-22 15:25:07 -0400

    :sparkles: (root resolvers) using appId that's passed from parent to make API requests

--- src/resolvers/five-day-forecast.js
+++ src/resolvers/five-day-forecast.js
@@ -1,23 +1,23 @@
 const { addUnits, get } = require('../utils');
-
-const APPID = process.env.APP_ID
-const CURRENT_FORECAST_URL = `https://api.openweathermap.org/data/2.5/forecast?${APPID}`
+const CURRENT_FORECAST_URL = `https://api.openweathermap.org/data/2.5/forecast`;
 
 module.exports = {
-  byCityName: (_, { name, countryCode, units }) => {
-    const location = [name]
+  byCityName: ({ appId }, { name, countryCode, units }) => {
+    const location = [name];
     if (countryCode && countryCode.trim()) {
-      location.push(countryCode.trim())
+      location.push(countryCode.trim());
     }
-    return get(`${CURRENT_FORECAST_URL}&q=${location.join()}${addUnits(units)}`)
+    return get(`${CURRENT_FORECAST_URL}?APPID=${appId}&q=${location.join()}${addUnits(units)}`);
   },
-  byCityID: (_, { id, units }) => get(`${CURRENT_FORECAST_URL}&id=${id}${addUnits(units)}`),
-  byLatLon: (_, { lat, lon, units }) => get(`${CURRENT_FORECAST_URL}&lat=${lat}&lon=${lon}${addUnits(units)}`),
-  byZIP: (_, { zip, countryCode, units }) => {
-    const location = [zip]
+  byCityID: ({ appId }, { id, units }) =>
+    get(`${CURRENT_FORECAST_URL}?${appId}&id=${id}${addUnits(units)}`),
+  byLatLon: ({ appId }, { lat, lon, units }) =>
+    get(`${CURRENT_FORECAST_URL}?APPID=${appId}&lat=${lat}&lon=${lon}${addUnits(units)}`),
+  byZIP: ({ appId }, { zip, countryCode, units }) => {
+    const location = [zip];
     if (countryCode && countryCode.trim()) {
-      location.push(countryCode.trim())
+      location.push(countryCode.trim());
     }
-    return get(`${CURRENT_FORECAST_URL}&zip=${location.join()}${addUnits(units)}`)
+    return get(`${CURRENT_FORECAST_URL}?APPID=${appId}&zip=${location.join()}${addUnits(units)}`);
   }
-}
+};
