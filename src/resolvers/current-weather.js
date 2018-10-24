ce015c9ebf6dfdeed84678d306721f4d5e0a72fe
Dustin Schie <dustin@schie.io>
2018-10-24 15:53:41 -0400

    :sparkles: adding multilingual support

--- src/resolvers/current-weather.js
+++ src/resolvers/current-weather.js
@@ -1,24 +1,36 @@
-const { addUnits, get } = require('../utils');
+const { addUnits, get, addLanguage } = require('../utils');
 
 const CURRENT_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 module.exports = {
-  byCityName: ({ appId }, { name, countryCode, units }) => {
+  byCityName: ({ appId, lang }, { name, countryCode, units }) => {
     const location = [name];
     if (countryCode && countryCode.trim()) {
       location.push(countryCode.trim());
     }
-    return get(`${CURRENT_WEATHER_URL}?APPID=${appId}&q=${location.join()}${addUnits(units)}`);
+    return get(
+      `${CURRENT_WEATHER_URL}?APPID=${appId}&q=${location.join()}${addUnits(units)}${addLanguage(
+        lang
+      )}`
+    );
   },
-  byCityID: ({ appId }, { id, units }) =>
-    get(`${CURRENT_WEATHER_URL}?APPID=${appId}&id=${id}${addUnits(units)}`),
-  byLatLon: ({ appId }, { lat, lon, units }) =>
-    get(`${CURRENT_WEATHER_URL}?APPID=${appId}&lat=${lat}&lon=${lon}${addUnits(units)}`),
-  byZIP: ({ appId }, { zip, countryCode, units }) => {
+  byCityID: ({ appId, lang }, { id, units }) =>
+    get(`${CURRENT_WEATHER_URL}?APPID=${appId}&id=${id}${addUnits(units)}${addLanguage(lang)}`),
+  byLatLon: ({ appId, lang }, { lat, lon, units }) =>
+    get(
+      `${CURRENT_WEATHER_URL}?APPID=${appId}&lat=${lat}&lon=${lon}${addUnits(units)}${addLanguage(
+        lang
+      )}`
+    ),
+  byZIP: ({ appId, lang }, { zip, countryCode, units }) => {
     const location = [zip];
     if (countryCode && countryCode.trim()) {
       location.push(countryCode.trim());
     }
-    return get(`${CURRENT_WEATHER_URL}?APPID=${appId}&zip=${location.join()}${addUnits(units)}`);
+    return get(
+      `${CURRENT_WEATHER_URL}?APPID=${appId}&zip=${location.join()}${addUnits(units)}${addLanguage(
+        lang
+      )}`
+    );
   }
 };
